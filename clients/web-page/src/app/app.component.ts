bb00ad0c2ec3a5859359d8e3da71363bb8bd6e23
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2020-05-21 13:14:31 -0600

    [Update] - Web page. Interfaz básica terminada
    1. Se carga una imagen a la vez
    2. Se muestra la imagen cargada
    3. Se escoge el tipo de manipulación que se le aplicará a la image, filtrado o clasificación

--- clients/web-page/src/app/app.component.ts
+++ clients/web-page/src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+declare interface Type {
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'web-page';
+  public base64Files: string[] = [];
+  private fileReader = new FileReader();
+  public loading: boolean = false;
+  public source: string = 'https://boofcv.org/images/thumb/6/66/Kodim17_noisy.jpg/300px-Kodim17_noisy.jpg'
+  public title: string = 'Image Filter and Classifier';
+  public type: Type;
+
+  constructor() {
+    this.type = {
+      name: 'Filter'
+    };
+  }
+  public onChange(event: Event) {
+    let files = event.target['files'];
+    this.loading = true;
+
+    if (files) {
+      this.fileReader.onload = () => {
+        this.source = this.fileReader.result.toString();
+        this.loading = false;
+      };
+  
+      this.fileReader.readAsDataURL(files[0]);
+      console.log(this.base64Files, this.fileReader)
+    }
+  };
 }
--- clients/web-page/src/app/app.module.ts
+++ clients/web-page/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +11,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
